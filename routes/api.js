f53ccc76a4da13df5ed99eeca4003f6e5a0503db
Michael <michaelshomefolder@Michaels-Laptop.local>
2022-12-26 11:50:28 +0800

    feat: user can return existing book

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 'use strict';
+
+const { ObjectId } = require('mongodb');
 const bookModel = require('../database/models').Book;
 /*
 *
@@ -13,23 +15,34 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      //get all books
+      bookModel.find({}).then(function (books) {
+        res.send(books);
+        return;
+        });
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
-      if (title === "") {res.end("missing required field title");}
+      // check if data exists
+      if (!title) {
+        res.end("missing required field title");
+        return;
+      }
       // create new Book
       const newBook = new bookModel({title: title});
       // safe new book
-      newBook.save((err, data) => {
+        newBook.save((err, data) => {
         // check error
-        if (err || !data) {
-          res.send("missing required field comment");
-        } else {
-          res.json({ _id: newBook._id, title: newBook.title, });
-        }
-      });
+          if (err || !data) {
+            res.send("missing required field comment");
+            return;
+          } else {
+            res.json({ _id: newBook._id, title: newBook.title, });
+            return;
+          }
+        });
     })
     
     .delete(function(req, res){
@@ -40,8 +53,21 @@ module.exports = function (app) {
 
   app.route('/api/books/:id')
     .get(function (req, res){
-      let bookid = req.params.id;
+      let bookid = ObjectId(req.params.id);
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      // Find a book by their _id
+      bookModel.findById(bookid, (err, book) => {
+      if (err) {
+        console.error(err);
+        res.send('An error occurred');
+      } else if (book) {
+      // Send the data if the book exists
+      res.send(book);
+      } else {
+      // Send a message if the book does not exist
+      res.send("no book exists");
+      }
+    });
     })
     
     .post(function(req, res){
